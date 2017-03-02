ccd9085ca539d7e8c0f88df97fa5a8de7ca66f0e
kodayv <kodayv@hawaii.edu>
2017-03-01 15:50:53 -1000

    Task 5: Events, Sessions

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,4 +1,31 @@
 /**
  * Created by koday on 2/28/2017.
  */
-console.log("Hello world");
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  console.log("Hello Client");
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+
+    }
+  });
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello Server");
+}
